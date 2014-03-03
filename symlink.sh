e486c23f37b2f18c35f62c753cdd4b0ae9eba8d7
Glass_saga <glass.saga@gmail.com>
2014-03-03 16:16:35 +0900

    debug print

--- symlink.sh
+++ symlink.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 source_path=`readlink -f $0`
 source_dir=`dirname $source_path`
+echo $source_path > /var/log/foo.log
 cd $source_dir
 for dotfile in `find .* -maxdepth 0`; do
   if [ $dotfile != '.' ] && [ $dotfile != '..' ] && [ $dotfile != '.git' ]
