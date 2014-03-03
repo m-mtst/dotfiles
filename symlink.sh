b131b91d3d2b9041169b03ced1d46aba85f9e925
Glass_saga <glass.saga@gmail.com>
2014-03-03 16:05:04 +0900

    sh

--- symlink.sh
+++ symlink.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
-cd $_
+source_path=`readlink -f $0`
+source_dir=`dirname $source_path`
+cd $source_dir
 for dotfile in `find .* -maxdepth 0`; do
   if [ $dotfile != '.' ] && [ $dotfile != '..' ] && [ $dotfile != '.git' ]
   then
-    ln -Ffs "$_/$dotfile" $HOME
+    ln -Ffs "$source_dir/$dotfile" $HOME
   fi
 done
