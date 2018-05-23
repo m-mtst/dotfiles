3fe5d0d4908dd0625007b2471ca871b8154a47c6
Masaki Matsushita <glass.saga@gmail.com>
2018-05-23 14:17:53 +0100

    update

--- symlink.sh
+++ symlink.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+
+set -e -u -x
+
 source_dir=$(cd $(dirname ${BASH_SOURCE:-$0}); pwd)
 for dotfile in `find .* -maxdepth 0`; do
   if [ $dotfile != '.' ] && [ $dotfile != '..' ] && [ $dotfile != '.git' ]
